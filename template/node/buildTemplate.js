65ffc164ab14d62906e21abccd930edcf635e665
Kurt Lourens <hi@kurtlourens.com>
2024-08-27 17:10:33 +0200

    🐛 Fix page generation

--- template/node/buildTemplate.js
+++ template/node/buildTemplate.js
@@ -106,7 +106,7 @@ async function buildTemplates() {
 
     const generatedGitIgnoreFiles = [];
 
-    let previousEventMedia = projectData.eventMedia.find(evm => evm.year === (projectData.event.current.year).toString());
+    let previousEventMedia = projectData.eventMedia.find(evm => evm.year === (projectData.event.current.year - 1).toString());
     if (previousEventMedia != null) {
         previousEventMedia = {
             ...previousEventMedia,
